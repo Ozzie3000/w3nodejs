c767e5a5b7da78acd6a5d2f1a3a7d39998868f96
Ozzie3000 <ozzieboy19@hotmail.com>
2017-08-23 23:18:37 -0400

    home.js

--- tutorial/home.js
+++ tutorial/home.js
@@ -1,5 +1,10 @@
 var http = require("http")
 
-http.createServer(function function_name(argument) {
+//http.createServer(function function_name(argument) {
 	// body...
-})
+//})
+
+http.createServer(function (req, res){
+	res.writeHead(200, {"Content-Type": "text/plain"});
+	res.end("Hello World");
+}).listen(8080);
