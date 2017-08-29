9475b29d80f0874ee75fdf9f6d887893e5260de5
Ozzie3000 <ozzieboy19@hotmail.com>
2017-08-29 16:22:11 -0400

    home.

--- tutorial/home.js
+++ tutorial/home.js
@@ -8,3 +8,8 @@ http.createServer(function (req, res){
 	res.writeHead(200, {"Content-Type": "text/plain"});
 	res.end("Hello World");
 }).listen(8080);
+
+
+//console logging
+console.log('Console stuff stuff');
+console.log('You should be seeing this in the CML');
