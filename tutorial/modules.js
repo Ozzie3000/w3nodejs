9ba81b2f3ef026df3ff04d3c202ff20a74fb8d99
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-01 16:47:05 -0400

    modules

---
+++ tutorial/modules.js
@@ -0,0 +1,1 @@
+//to use a module use the require() function
--- tutorial/second.js
+++ tutorial/second.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('What a World.  Love me some NodeJS');
+    res.end('What a World.  Love me some NodeJS. <br/> Port 3000');
 }).listen(3000);
