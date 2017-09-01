0355f7df51c34a639478130c206be9429ba68ddc
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-01 16:48:29 -0400

    modules

--- tutorial/modules.js
+++ tutorial/modules.js
@@ -1,1 +1,7 @@
 //to use a module use the require() function
+var http = require("http")
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('This is text in the document');
+}).listen(8080);
