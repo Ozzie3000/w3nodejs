db024c1fd650dc79561d3ea6ef016b9fbaf71872
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-02 21:36:28 -0400

    modules

--- tutorial/modules.js
+++ tutorial/modules.js
@@ -1,7 +1,13 @@
 //to use a module use the require() function
 var http = require("http")
 
+//a module to return date time
+exports.myDateTime = function () {
+    return Date();
+};
+
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('This is text in the document');
 }).listen(8080);
+
