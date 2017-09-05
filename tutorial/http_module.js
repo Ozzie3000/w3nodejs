0e354c87c1c576b433edaba3e8ad792739886885
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-04 20:16:53 -0400

    http module

--- tutorial/http_module.js
+++ tutorial/http_module.js
@@ -2,5 +2,10 @@
 //THis allows nodejs to transfer over http
 var http = require('http');
 
-
 //The HTTP module can create an HTTP server which listens to server ports and gives a response back to the client.
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+}).listen(8080); //the server object listens on port 8080
