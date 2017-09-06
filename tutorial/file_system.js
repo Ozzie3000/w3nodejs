419bd3afa9e8863c085cc0facf3b32164d5031cc
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-05 22:32:25 -0400

    file system

--- tutorial/file_system.js
+++ tutorial/file_system.js
@@ -1,2 +1,4 @@
 // The Node.js file system module allow you to work with the file system on your computer.
 // To include the File System module, use the require() method:
+
+var fs = require('fs');
