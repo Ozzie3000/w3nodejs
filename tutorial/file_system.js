7409037577f0b1e3172d83098222c8c2b7488609
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-07 07:27:20 -0400

    file system. demo file

--- tutorial/file_system.js
+++ tutorial/file_system.js
@@ -3,3 +3,13 @@
 // common uses: Read files, Create files, Update files, Delete files, Rename files
 var http = require('http');
 var fs = require('fs');
+
+
+//
+http.createServer(function (req, res) {
+  fs.readFile('demo1.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
+}).listen(8080);
