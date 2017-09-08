728de74b23bf4f029e59e2e53809d45fd6615e65
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-08 08:35:12 -0400

    url module

--- tutorial/url_module.js
+++ tutorial/url_module.js
@@ -1,2 +1,5 @@
 //Built-in URL module
 //To use the URL module use this method: require()
+//This module breaks up the web address into useable parts
+//To parse an address use url.parse()
+//It returns an URL object parts of the address as properties
