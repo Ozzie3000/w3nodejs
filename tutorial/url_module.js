07df71e5a4f958bbe6d8fbc5d00f95da310aa0ed
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-11 06:27:54 -0400

    url module

--- tutorial/url_module.js
+++ tutorial/url_module.js
@@ -2,4 +2,8 @@
 //To use the URL module use this method: require()
 //This module breaks up the web address into useable parts
 //To parse an address use url.parse()
-//It returns an URL object parts of the address as properties
+//It returns an URL object parts of the address as properties\
+
+var url = require('url');
+var adr = 'http://localhost:8080/default.htm?year=2017&month=february';
+var q = url.parse(adr, true);
