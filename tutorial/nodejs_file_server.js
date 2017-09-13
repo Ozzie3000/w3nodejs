9b268b79061cdc07b8371c99363bda5b20ea7e01
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-13 08:33:10 -0400

    file server

--- tutorial/nodejs_file_server.js
+++ tutorial/nodejs_file_server.js
@@ -1,3 +1,6 @@
 // Nodejs behaves like a filer server
 //and we know how to query a string
 //Let us put them together
+//make to more files and put them in same directory
+//Next we will make a separate file to open the requested file and return content to the requester
+//throw an error of type 404 if something doesn't work
