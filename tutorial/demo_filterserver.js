ca19bca70d58ff40b4bfb14d1a1347a0a35a16c3
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-17 23:15:04 -0400

    demo server for both spring and summer

--- tutorial/demo_filterserver.js
+++ tutorial/demo_filterserver.js
@@ -1,3 +1,8 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+  var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data)
