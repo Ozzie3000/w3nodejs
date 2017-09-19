190abe75ce0bcc28201c959083dc47cb562d8250
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-18 21:51:23 -0400

    demo server for both spring and summer

--- tutorial/demo_filterserver.js
+++ tutorial/demo_filterserver.js
@@ -5,4 +5,9 @@ var fs = require('fs');
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
   var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data)
+  fs.readFile(filename, function(err, data){
+  	if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+  }
