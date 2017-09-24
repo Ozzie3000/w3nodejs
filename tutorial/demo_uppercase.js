80bffb71a3eeb6901ea6c4caeb447ba7dba89471
Ozzie3000 <ozzieboy19@hotmail.com>
2017-09-23 22:38:10 -0400

    upper case module

--- tutorial/demo_uppercase.js
+++ tutorial/demo_uppercase.js
@@ -1,5 +1,6 @@
 //npm is a package manager for node.js
 //npm gets installed when you install node.js
+//what we expect to see is 'hello world' string to use the upper-case model
 
 var http = require('http');
 var uc = require('upper-case');
