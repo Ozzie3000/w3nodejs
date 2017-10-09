cb71fe04e51e3d3c64c1d0883c4174430a2fe0ee
Ozzie3000 <ozzieboy19@hotmail.com>
2017-10-08 21:58:48 -0400

    send html

--- tutorial/send_html.js
+++ tutorial/send_html.js
@@ -1,1 +1,6 @@
-send_html.js
+var mailOptions = {
+  from: 'youremaildude@gmail.com',
+  to: 'myfriendsemail@yahoo.com',
+  subject: 'Sending Email using Node.js',
+  html: '<h1>Welcome</h1><p>That was fun!</p>'
+}
